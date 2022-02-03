c4f82dcbb550ac7e4bc81126ad540de8f40bccdb
Иван Братин <bratinivan@mail.ru>
2022-02-03 22:18:04 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 100;  // счет клиента
+        int refill = 1100;  // пополнение счета клиента
+        int bonus = 0;  // бонус
+        bonus = refill >= 1000 ? refill /100 : bonus;
+        System.out.println("Итоговый счёт: " + (customerAccount + refill +bonus));
+        System.out.println("Бонусные рубли: " + bonus);
+    }
+}
