37c9dcf8ef81c2d4ac84fe1fe2edcbf5764f328e
bratinivan <96741166+bratinivan@users.noreply.github.com>
2022-02-11 17:10:24 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int customerAccount = 100;  // счет клиента
         int refill = 1100;  // пополнение счета клиента
         int bonus = 0;  // бонус
-        bonus = refill >= 1000 ? refill /100 : bonus;
+        bonus = refill >= 1000 ? refill / 100 : bonus;
         System.out.println("Итоговый счёт: " + (customerAccount + refill + bonus));
         System.out.println("Бонусные рубли: " + bonus);
     }
